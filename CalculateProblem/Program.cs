57432e86880799840e74082357bffd47612c803a
SupportExTraGoZ <stranged96@gmail.com>
2018-07-04 12:39:36 +0300

    Optimizing & Cleanup

--- CalculateProblem/Program.cs
+++ CalculateProblem/Program.cs
@@ -6,39 +6,17 @@ namespace CalculateProblem
     {
         static void Main()
         {
-            float N = Convert.ToSingle(Console.ReadLine());
-            float x = Convert.ToSingle(Console.ReadLine());
-            if (N < 2 || N > 10) return;
-            if (x < 0.5f || x > 100) return;
-            var Factorials = CalculateFactorial(new float[] {N, x});
-            var Sum = Calculate(Factorials[0], Factorials[1], N, x);
-            Console.WriteLine(Sum);
-        }
+            long N = Convert.ToInt32(Console.ReadLine());
+            double x = Convert.ToDouble(Console.ReadLine());
 
-        static float[] CalculateFactorial(float[] Nx)
-        {
-            float firstFactorial = 1;
-            float secondFactorial = 1;
-            for (int i = 1; i <= Math.Max(Nx[0], Nx[1]); i++)
+            long tempFact = 1;
+            double Sum = 1;
+            for (int i = 1; i <= N; i++)
             {
-                if (i <= Nx[0])
-                {
-                    firstFactorial = firstFactorial * i;
-                }
-                if (i <= Nx[1])
-                {
-                    secondFactorial = secondFactorial * i;
-                }
+                tempFact = tempFact * i;
+                Sum += tempFact / Math.Pow(x, i);
             }
-            return new float[] { firstFactorial, secondFactorial };
-        }
-
-        static string Calculate(float NFactorial, float xFactorial, float N = 0, float x = 0)
-        {
-            double Sum;
-            // S formula
-            Sum = (1 + 1 / x + 2 / Math.Pow(x, 2)) + (NFactorial / Math.Pow(x, N));
-            return Sum.ToString("0.00000");
+            Console.WriteLine(string.Format("{0:F5}", Sum));
         }
     }
 }
