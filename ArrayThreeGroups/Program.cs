e4c90db26bca9f02314db7b6c2b741c16b53e97b
SupportExTraGoZ <stranged96@gmail.com>
2018-07-05 21:37:23 +0300

    ThreeGroups Task Completed with a little bit of help

---
+++ ArrayThreeGroups/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace ArrayThreeGroups
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i <= 2; i++)
+            {
+                int[] line = nums.Where(x => x % 3 == i).ToArray();
+                Console.WriteLine(string.Join(" ", line));
+            }
+        }
+    }
+}
