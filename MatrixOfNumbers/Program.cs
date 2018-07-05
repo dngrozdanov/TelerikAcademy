2fa35d87a4c935b90c19627c13214128d57b7da7
SupportExTraGoZ <stranged96@gmail.com>
2018-07-05 16:25:09 +0300

    Matrix Of Numbers Task Completed

---
+++ MatrixOfNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MatrixOfNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= N; i++)
+            {
+                for (int j = i; j < N + i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
