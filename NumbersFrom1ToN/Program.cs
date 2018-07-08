0bf6fccdfe3807be2fe86fe0b1a7e9242dfdc3e7
SupportExTraGoZ <SupportExTraGoZ@users.noreply.github.com>
2018-07-08 22:54:30 +0300

    NumbersFrom1ToN Task

---
+++ NumbersFrom1ToN/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        short N = short.Parse(Console.ReadLine());
+        for (short i = 1; i <= N; i++)
+        {
+            Console.Write(i + " ");
+        }
+    }
+}
