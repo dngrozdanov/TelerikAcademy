1a9b1706fa5cfd03bdb9083b18c7de67b1625a91
SupportExTraGoZ <stranged96@gmail.com>
2018-07-10 17:41:27 +0300

    SortThreeNumbers

---
+++ SortThreeNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SortThreeNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            int first = Convert.ToInt32(Console.ReadLine());
+            int second = Convert.ToInt32(Console.ReadLine());
+            int third = Convert.ToInt32(Console.ReadLine());
+    
+            if (first < second)
+                Swap(ref first, ref second);
+            if (second < third)
+                Swap(ref second, ref third);
+            if (first < second)
+                Swap(ref first, ref second);
+    
+            Console.WriteLine($"{first} {second} {third}");
+        }
+    
+        static void Swap(ref int x, ref int y)
+        {
+            x = x ^ y;
+            y = x ^ y;
+            x = x ^ y;
+        }
+    }
+}
