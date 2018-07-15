e93b263f100c5a27eac1f1b608047fd5aac2e1ac
SupportExTraGoZ <stranged96@gmail.com>
2018-07-15 23:43:39 +0300

    Projects Completed

---
+++ BiggestOfFive/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace BiggestOfFive
+{
+    class Program
+    {
+        static void Main()
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            double d = double.Parse(Console.ReadLine());
+            double e = double.Parse(Console.ReadLine());
+            double[] doubleArray = { a, b, c, d, e };
+            double finalMax = doubleArray.Max();
+            if (finalMax == (int)finalMax)
+            {
+                Console.WriteLine((int)finalMax);
+            }
+            else
+            {
+                Console.WriteLine(finalMax);
+            }
+        }
+    }
+}
---
+++ BiggestOfThree/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BiggestOfThree
+{
+    class Program
+    {
+        static void Main()
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            double finalMax = Math.Max(Math.Max(a, b), c);
+            if (finalMax == (int)finalMax)
+            {
+                Console.WriteLine((int)finalMax);
+            }
+            else
+            {
+                Console.WriteLine(finalMax);
+            }
+        }
+    }
+}
