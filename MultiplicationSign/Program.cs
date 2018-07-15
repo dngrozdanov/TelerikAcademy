52a30002eb7e1136faf7805ccf8ead1bd983403e
SupportExTraGoZ <stranged96@gmail.com>
2018-07-15 20:53:31 +0300

    Projects Completed

---
+++ MultiplicationSign/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MultiplicationSign
+{
+    class Program
+    {
+        static void Main()
+        {
+            double a = double.Parse(Console.ReadLine()), b = double.Parse(Console.ReadLine()), c = double.Parse(Console.ReadLine());
+            int d = 0;
+            if (a == 0 || b == 0 || c == 0)
+            {
+                Console.WriteLine("0");
+                return;
+            }
+            if (a < 0)
+                d++;
+            if (b < 0)
+                d++;
+            if (c < 0)
+                d++;
+            if (d % 2 == 0)
+                Console.WriteLine("+");
+            else
+                Console.WriteLine("-");
+        }
+    }
+}
---
+++ NonDividable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        short[] arrayOfNumbers = new short[1];
+        arrayOfNumbers = Console.ReadLine().Split().Select(short.Parse).ToArray();
+        for (short i = 2; i <= (short)100; i++)
+        {
+            while (arrayOfNumbers[0] % i == (short)0 && arrayOfNumbers[1] % i == 0)
+            {
+                arrayOfNumbers[0] = (short)(arrayOfNumbers[0] / i);
+                arrayOfNumbers[1] = (short)(arrayOfNumbers[1] / i);
+            }
+        }
+        Console.WriteLine(String.Join(" ", arrayOfNumbers));
+    }
+}
