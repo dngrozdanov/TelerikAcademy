c5062976e2d99ccc979d238fc6cadeaaded0681a
SupportExTraGoZ <stranged96@gmail.com>
2018-07-16 14:08:38 +0300

    HIDDEN Bunny Task Reformed

--- HIDDENTASK_JOROTHENAUGHTY/Program.cs
+++ HIDDENTASK_JOROTHENAUGHTY/Program.cs
@@ -11,61 +11,55 @@ namespace HIDDENTASK_JOROTHENAUGHTY
         static int[,] Matrix;
         static void Main()
         {
-            try
+            int[] RowsColsJumps = Console.ReadLine().Trim().Split().Select(int.Parse).ToArray();
+            int[] StartPositionXY = Console.ReadLine().Trim().Split().Select(int.Parse).ToArray();
+            Dictionary<int, Tuple<int, int>> Jumps = new Dictionary<int, Tuple<int, int>>();
+            for (int i = 0; i < RowsColsJumps[2]; i++)
             {
-                int[] RowsColsJumps = Console.ReadLine().Trim().Split().Select(int.Parse).ToArray();
-                int row = RowsColsJumps[0];
-                int col = RowsColsJumps[1];
-                int[] StartPositionXY = Console.ReadLine().Trim().Split().Select(int.Parse).ToArray();
-                Dictionary<int, Tuple<int, int>> Jumps = new Dictionary<int, Tuple<int, int>>();
-                for (int i = 0; i < RowsColsJumps[2]; i++)
-                {
-                    int[] XY = Console.ReadLine().Trim().Split().Select(int.Parse).ToArray();
-                    Jumps.Add(i, new Tuple<int, int>(XY[0], XY[1]));
-                }
-                Matrix = new int[row, col];
+                int[] XY = Console.ReadLine().Trim().Split().Select(int.Parse).ToArray();
+                Jumps.Add(i, new Tuple<int, int>(XY[0], XY[1]));
+            }
 
-                int indexNum = 1;
-                for (int i = 0; i < row; i++)
-                {
-                    for (int y = 0; y < col; y++)
-                    {
-                        Matrix[i, y] = indexNum;
-                        indexNum++;
-                    }
-                }
+            int row = RowsColsJumps[0];
+            int col = RowsColsJumps[1];
+            Matrix = new int[row, col];
 
-                int[] currentPosition = { StartPositionXY[0], StartPositionXY[1] };
-                CollectSum(currentPosition);
-
-                while (true)
+            int indexNum = 1;
+            for (int i = 0; i < row; i++)
+            {
+                for (int y = 0; y < col; y++)
                 {
-                    for (int i = 0; i < Jumps.Count; i++)
-                    {
-                        var tempJump = Jumps[i];
-                        currentPosition[0] += tempJump.Item1;
-                        currentPosition[1] += tempJump.Item2;
-                        if (currentPosition[0] >= row || currentPosition[1] >= col || currentPosition[0] < 0 || currentPosition[1] < 0)
-                        {
-                            Console.WriteLine($"escaped {Sum}");
-                            Environment.Exit(0);
-                        }
-                        else
-                        {
-                            if (Matrix[currentPosition[0], currentPosition[1]] == 0)
-                            {
-                                Console.WriteLine($"caught {JumpsDone}");
-                                Environment.Exit(0);
-                            }
-                            CollectSum(currentPosition);
-                            JumpsDone++;
-                        }
-                    }
+                    Matrix[i, y] = indexNum;
+                    indexNum++;
                 }
             }
-            catch (Exception e)
+
+            int[] currentPosition = { StartPositionXY[0], StartPositionXY[1] };
+            CollectSum(currentPosition);
+
+            while (true)
             {
-                Console.WriteLine(e.Message);
+                for (int i = 0; i < Jumps.Count; i++)
+                {
+                    var tempJump = Jumps[i];
+                    currentPosition[0] += tempJump.Item1;
+                    currentPosition[1] += tempJump.Item2;
+                    if (currentPosition[0] >= row || currentPosition[1] >= col || currentPosition[0] < 0 || currentPosition[1] < 0)
+                    {
+                        Console.WriteLine($"escaped {Sum}");
+                        Environment.Exit(0);
+                    }
+                    else
+                    {
+                        if (Matrix[currentPosition[0], currentPosition[1]] == 0)
+                        {
+                            Console.WriteLine($"caught {JumpsDone}");
+                            Environment.Exit(0);
+                        }
+                        CollectSum(currentPosition);
+                        JumpsDone++;
+                    }
+                }
             }
         }
 
