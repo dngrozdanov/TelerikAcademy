f6a18c2799c8f7191b2a855362707cec0f3693a3
SupportExTraGoZ <stranged96@gmail.com>
2018-07-17 10:03:27 +0300

    PrimeTriangle & BiggestOfThree

--- PrimeTriangle/Program.cs
+++ PrimeTriangle/Program.cs
@@ -9,29 +9,24 @@ namespace PrimeTriangleWIP
         {
             int Number = Convert.ToInt32(Console.ReadLine());
             List<int> PrimeNumbers = new List<int>();
-            for (int i = 0; i < Number; i++)
+            for (int i = 1; i <= Number; i++)
             {
-                if (IsPrime(i) || i == 1 || i == 2)
+                if (IsPrime(i))
                 {
-                    PrimeNumbers.Add(i);
+                    PrimeNumbers.Add(1);
                 }
-            }
-
-            for (int j = 0; j < PrimeNumbers.Count; j++)
-            {
-                for (int x = 1; x <= PrimeNumbers[j]; x++)
+                else
                 {
-                    if (IsPrime(x) || x == 1 || x == 2)
-                        Console.Write(1);
-                    else
-                        Console.Write(0);
+                    PrimeNumbers.Add(0);
+                    continue;
                 }
-                Console.WriteLine();
+                Console.WriteLine(string.Join("", PrimeNumbers));
             }
         }
 
         public static bool IsPrime(int number)
         {
+            if (number == 1) return true;
             if (number < 2) return false;
             if (number % 2 == 0) return number == 2;
             var root = (int)Math.Sqrt(number);
