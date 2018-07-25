b94d47d533986d2f4fc4ee24366e594f8ec9eed9
SupportExTraGoZ <stranged96@gmail.com>
2018-07-25 10:09:12 +0300

    Student Class improvements...

---
+++ SchoolOOP/Contracts/IStudent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using SchoolOOP.Humans;
+
+namespace SchoolOOP.Contracts
+{
+    public interface IStudent
+    {
+        int Grade { get; set; }
+        int ClassNumber { get; set; }
+    }
+}
--- SchoolOOP/Humans/Student.cs
+++ SchoolOOP/Humans/Student.cs
@@ -1,13 +1,20 @@
 ﻿using System;
+using SchoolOOP.Contracts;
+
 namespace SchoolOOP.Humans
 {
-    public class Student : Human
+    public class Student : Human, IStudent
     {
         private int grade;
+        private int classnumber;
+
         public int Grade { get { return this.grade; } set { if (value <= 0) throw new ArgumentOutOfRangeException("Grade cannot be less than or equal to zero."); this.grade = value; } }
-        public Student(string firstName, string lastName, int grade) : base(firstName, lastName)
+        public int ClassNumber { get { return this.classnumber; } set { if (value <= 0) throw new ArgumentOutOfRangeException("Class number cannot be less than or equal to zero."); this.classnumber = value; } }
+
+        public Student(string firstName, string lastName, int grade, int classnumber) : base(firstName, lastName)
         {
             this.Grade = grade;
+            this.ClassNumber = classnumber;
         }
     }
 }
